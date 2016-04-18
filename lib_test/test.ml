953326f51a815b5edd98ca2e8d2f92e7b5f8e90e
Jeremy Yallop <yallop@gmail.com>
2016-04-18 14:42:56 +0100

    Add tests for Sys_stat.Mode.of_string.

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -69,10 +69,7 @@ end
 
 module Mode =
 struct
-  let test_to_string_linux () =
-    let host = Sys_stat_host.Linux.V4_1_12.Musl.v1_1_12 in
-    let to_string = Sys_stat.Mode.to_string ~host:host.Sys_stat.Host.mode in
-    let tests = Sys_stat.File_kind.([
+  let string_test_cases = Sys_stat.File_kind.([
       DIR , 0o001, "d--------x";
       CHR , 0o010, "c-----x---";
       BLK , 0o100, "b--x------";
@@ -82,17 +79,32 @@ struct
       SOCK, 0o004, "s------r--";
       SOCK, 0o040, "s---r-----";
       SOCK, 0o400, "sr--------";
-    ]) in
-    ListLabels.iter tests
+    ])
+
+  let test_of_string_linux () =
+    let host = Sys_stat_host.Linux.V4_1_12.Musl.v1_1_12 in
+    let of_string = Sys_stat.Mode.of_string ~host:host.Sys_stat.Host.mode in
+    ListLabels.iter string_test_cases
+      ~f:(fun (expected_kind, expected_perm, string) ->
+          Printf.ksprintf Alcotest.(check bool)
+            "%s parses as (%s, 0o%o)"
+            string (Sys_stat.File_kind.to_string expected_kind) expected_perm
+            true
+            (of_string string = (expected_kind, expected_perm)))
+
+  let test_to_string_linux () =
+    let host = Sys_stat_host.Linux.V4_1_12.Musl.v1_1_12 in
+    let to_string = Sys_stat.Mode.to_string ~host:host.Sys_stat.Host.mode in
+    ListLabels.iter string_test_cases
       ~f:(fun (kind, perm, expected) ->
           Printf.ksprintf Alcotest.(check string)
             "(%s, Oo%o) prints as %s"
             (Sys_stat.File_kind.to_string kind) perm expected
             expected
             (to_string (kind, perm)))
-      
 
   let tests = [
+    "of_string", `Quick, test_of_string_linux;
     "to_string", `Quick, test_to_string_linux;
   ]
 end
